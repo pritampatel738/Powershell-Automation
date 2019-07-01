22263d5f47e59de7f0241dcef80b2b05c46bd993
Pritam Patel <pritampatel738@gmail.com>
2019-07-01 04:27:37 -0700

    Delete python.py

--- PBITables/python.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Chrome("D:\drivers\chromedriver.exe")
-driver.get("https://www.google.com")
-
-driver.implicitlyWait(20)
-driver.close()
